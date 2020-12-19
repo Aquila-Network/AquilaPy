73a5903285057ee2f3f3b5eda2a51c5ea4121a26
freakeinstein <wow.dr.dre@gmail.com>
2020-12-19 18:24:13 +0530

    added deps to setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Aquila-Network/aquilapy",
     packages=setuptools.find_packages(),
-    install_requires=[],
+    install_requires=[
+    "bson",
+    "requests",
+    "pycryptodome",
+    "base58"
+    ],
     classifiers=[
       "Programming Language :: Python :: 3",
       "License :: OSI Approved :: MIT License",
